66f90bbe499bdae33164d389aac1325516eef774
xmaster <black.zone.mail.1000@gmail.com>
2018-02-03 20:42:57 +0200

    updates to more texts

--- bee.py
+++ bee.py
@@ -96,7 +96,9 @@ def heading():
                 .' '. I BEE YOU  __
        .        .   .          \(__\_/      Version: 2.3(+HTTP)
         .         .         . -{{#(|8)
-          ' .  . ' ' .  . '    /(__/ \      by:''' + WHITE + ' Alisson Moretto (' + YELLOW + '4w4k3' + WHITE + ')' + '\n' + '\n' + END) 
+          ' .  . ' ' .  . '    /(__/ \      by:''' + WHITE + ' Alisson Moretto (' + YELLOW + '4w4k3' + WHITE + ')' + YELLOW + '''
+                                            updated by:''' + WHITE + ' xmaster (' + YELLOW + 'black-zone-1000' + WHITE + ')' 
+          + '\n' + '\n' + END) 
           
     print ' {0}[{1}K{0}]{1} Generate Keylogger  {0}[{1}U{0}]{1} Update  {0}[{1}Q{0}]{1} Quit  '.format(YELLOW, WHITE) + '\n'
 def pp():
